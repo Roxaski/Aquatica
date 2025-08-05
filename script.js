77a6517be575f264e1f7cfd6d8d16e79ae629c86
Roxas Ski <roxaskiGithub@gmail.com>
2025-08-05 15:43:19 +0100

    updated comments

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let logo = document.querySelector('.logo');
 
-//reveal logo on scroll
+// The logo appears once the the user scrolls down more than 100px
 window.addEventListener('scroll', () => {
     if (window.scrollY > 100) {
         logo.classList.add("active");
     }
+    // the logo dissapears once the user scrolls back to top of page ( when it's less than 5px )
     else if (window.scrollY < 5) {
         logo.classList.remove("active");
     }
