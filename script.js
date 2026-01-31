71d058d5c633d690df0f3761ea9b383d0f9e982a
Roxas Ski <roxaskiGithub@gmail.com>
2026-01-31 12:17:48 +0000

    minor performance improvements

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let isLogoDisplaying;
-const displayLogo = document.querySelector('.logo');
+const displayTitle = document.querySelector('.logo');
 const fishingBoat = document.querySelector('.fishing-boat');
 const noFishing = document.querySelector('.no-fishing');
+const heroSection = document.querySelector('.hero');
+let titleVisible = false;
 
-function logoReveal() {
-    // contains the value of how much the window was scrolled
-    const scrollY = window.scrollY;
+// observes the hero section in order to show / hide the title 
+const logoObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting && !titleVisible) {
+            // when the hero is out of view adds an active class to show title
+            displayTitle.classList.add('active');
+            titleVisible = true;
+        } else if (entry.isIntersecting && titleVisible) {
+            // when the hero is back in view removes the active class to hide title
+            displayTitle.classList.remove('active');
+            titleVisible = false;
+        };
+    });
+}, {
+    threshold: .8
+});
 
-    // displays the logo when scrolling more than 100px, and hides the logo when returning to top of page
-    if (scrollY > 100 && !isLogoDisplaying) {
-        displayLogo.classList.add('active');
-        isLogoDisplaying = true;
-    } else if (scrollY < 5 && isLogoDisplaying) {
-        displayLogo.classList.remove('active');
-        isLogoDisplaying = false;
-    };
-};
-
-// calls the function on scroll
-window.addEventListener('scroll', logoReveal);
+// observes the hero section
+logoObserver.observe(heroSection);
 
 // triggers the animation when close to the element
 const animationLoading = new IntersectionObserver(elements => {
@@ -31,12 +35,12 @@ const animationLoading = new IntersectionObserver(elements => {
             animationLoading.unobserve(img.target);
         };
     });
-    // adds margin of 300px in order to trigger the animation 300px before the user gets to it
+    // adds a margin of 300px in order to trigger the animation 300px before the user gets to it
 }, {
     rootMargin: '0px 0px 300px 0px',
     threshold: 0
 });
 
-// these elements are being observed by intersectionObserver
+// observes the fishing boat elements
 animationLoading.observe(fishingBoat);
 animationLoading.observe(noFishing);
